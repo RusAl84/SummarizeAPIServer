9744b66f5b27f5d19add4832884dd51712dbc99c
Aleksey <rusal@bk.ru>
2022-02-02 21:15:07 +0300

    fix

--- app.py
+++ app.py
@@ -4,41 +4,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# toDOlist = []
-# toDoItem = {}
-# toDoItem['name'] = "Flask"
-# toDoItem['priority'] = 3
-# toDOlist.append(toDoItem)
-# toDOlist.append(toDoItem)
-# toDOlist.append(toDoItem)
-
-
-@app.route('/')
-def get_status():  # put application's code here
-    status = ""
-    for item in toDOlist:
-        status += f"name: {item['name']}   &nbsp; &nbsp; " \
-                  f" priority: {item['priority']}  </br> "
-    return status
-
 # https://docs-python.ru/packages/veb-frejmvork-flask-python/registratsija-marshrutov-url-adresov-flask/
 # https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def root():  # put application's code here
+    return "Автоматическая суммаризация текстов"
 
-@app.route('/get/<id>')
-def getToDO(id):
-    # if 0 <= int(id) < len(toDOlist):
-    #     item=toDOlist[int(id)]
-    #     # return f"name: {item['name']}   &nbsp; &nbsp; " \
-    #     #           f" priority: {item['priority']}  </br> "
-    #     return jsonify(item)
-    # else:
-    #     return jsonify(toDOlist)
-    return "Not Found"
-
-@app.route('/api/set', methods=['POST'])
+@app.route('/sum', methods=['POST'])
 def foo():
     text = request.json
     text = text['text']
@@ -59,10 +31,10 @@ def foo():
     print("Исходный текст:")
     print(text)
     print("Результат работы Rake:")
-
     print(str(mas))
     print(mas[0])
     return jsonify(mas), 200, {'Content-Type': 'application/json'}
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 app.run(host='0.0.0.0')
